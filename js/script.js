36e181e1ecd0ad3d08a5f3d1fd5fd28afe30e859
lorida0777 <kantonotsiferana@gmail.com>
2025-07-16 09:31:29 +0300

    arrow event listener

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ const activePortfolio = () => {
 arrowRight.addEventListener("click", () => {
   if (index < 4) {
     index++;
-    arrowLeft.classList.remove("disabled");
+    arrowRight.classList.remove("disabled");
   } else {
     index = 5;
     arrowRight.classList.add("disabled");
