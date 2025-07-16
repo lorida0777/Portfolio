2914829fbe2ff6777c3f655976f1ee0e4c23d340
lorida0777 <kantonotsiferana@gmail.com>
2025-07-16 12:16:25 +0300

    logolink

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const navLinks = document.querySelectorAll("header nav a");
+const logoLink = document.querySelector(".logo");
 
 const activePage = () => {
   navLinks.forEach((link) => {
@@ -13,6 +14,12 @@ navLinks.forEach((link, idx) => {
       link.classList.add("active");
     }
   });
+});
+
+logoLink.addEventListener("click", () => {
+  if (!navLinks[0].classList.contains("active")) {
+    activePage();
+  }
 });
 
 const resumeBtns = document.querySelectorAll(".resume-button");
