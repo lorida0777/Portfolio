32097f44a2c596fba3ea07c533a34ebe78c0259c
lorida0777 <kantonotsiferana@gmail.com>
2025-07-16 19:18:05 +0300

    const navbar

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ const navLinks = document.querySelectorAll("header nav a");
 const logoLink = document.querySelector(".logo");
 const sections = document.querySelectorAll("section");
 const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector("header nav");
 
 menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 });
 const activePage = () => {
   const header = document.querySelector("header");
