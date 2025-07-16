ce584fb6a2bc4693f5c150e190a3b63901c699d5
lorida0777 <kantonotsiferana@gmail.com>
2025-07-16 07:33:01 +0300

    Add service, resume

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const resumeBtns = document.querySelectorAll(".resume-button");
+
+resumeBtns.forEach((btn, idx) => {
+  btn.addEventListener("click", () => {
+    const removeDetails = document.querySelectorAll(".resume-detail");
+    resumeBtns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    btn.classList.add("active");
+
+    removeDetails.forEach((detail) => {
+      detail.classList.remove("active");
+    });
+    removeDetails[idx].classList.add("active");
+  });
+});
+
+const arrowRight = document.querySelector(
+  ".portfolio-box .navagation .arrow-right"
+);
+const arrowLeft = document.querySelector(
+  ".portfolio-box .navagation .arrow-left"
+);
+
+let index = 0;
+
+const activePortfolio = () => {
+  const imgSlide = document.querySelector(".portfolio-carousel .img-slide");
+
+  const portfolioDetails = document.querySelectorAll(".portfolio-detail");
+
+  imgSlide.style.transform = `translateX(calc(${index * -100}% - ${
+    index * 2
+  }rem))`;
+
+  portfolioDetails.forEach((detail) => {
+    detail.classList.remove("active");
+  });
+  portfolioDetails[index].classList.add("active");
+};
+
+arrowRight.addEventListener("click", () => {
+  if (index < 4) {
+    index++;
+    arrowLeft.classList.remove("disabled");
+  } else {
+    index = 5;
+    arrowRight.classList.add("disabled");
+  }
+
+  activePortfolio();
+});
+
+arrowLeft.addEventListener("click", () => {
+  if (index > 1) {
+    index--;
+    arrowLeft.classList.remove("disabled");
+  } else {
+    index = 0;
+    arrowLeft.classList.add("disabled");
+  }
+
+  activePortfolio();
+});
